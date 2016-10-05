fd0b23a353787f1603c3d47c7abf014660cea581
duanders <dustin.james.anderson@cern.ch>
2016-10-05 06:09:03 -0700

    move some parts of MPIDriver script to utils

--- mpi_learn/utils.py
+++ mpi_learn/utils.py
@@ -19,3 +19,29 @@ def get_num_gpus():
     driver.init()
     num_gpus = driver.Device.count()
     return num_gpus
+
+def import_keras(tries=10):
+    """There is an issue when multiple processes import Keras simultaneously --
+        the file .keras/keras.json is sometimes not read correctly.  
+        as a workaround, just try several times to import keras."""
+    for try_num in range(tries):
+        try:
+            import keras
+            return
+        except ValueError:
+            print "Unable to import keras. Trying again: %d" % try_num
+            sleep(0.1)
+    print "Failed to import keras!"
+
+def load_model(arch_file, weights_file=None):
+    """Loads model architecture from JSON and instantiates the model.
+        arch_file: JSON file specifying model architecture
+        weights_file: HDF5 file containing model weights"""
+    import_keras()
+    from keras.models import model_from_json
+    with open( arch_file ) as arch_f:
+        model = model_from_json( arch_f.readline() ) 
+    if weights_file is not None:
+        model.load_weights( weights_file )
+    return model
+
