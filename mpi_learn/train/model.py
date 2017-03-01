82fd7d8effb9e2054f7143e6b68364864e1b2160
duanders <dustin.james.anderson@cern.ch>
2017-02-28 19:41:46 -0800

    First attempt at multi-gpu running with TF

--- mpi_learn/train/model.py
+++ mpi_learn/train/model.py
@@ -1,6 +1,6 @@
 ### ModelBuilder class and associated helper methods
 
-from mpi_learn.utils import load_model
+from mpi_learn.utils import load_model, get_device_name
 
 class ModelBuilder(object):
     """Class containing instructions for building neural net models.
@@ -35,3 +35,47 @@ class ModelFromJson(ModelBuilder):
 
     def build_model(self):
         return load_model(filename=self.filename, json_str=self.json_str, custom_objects=self.custom_objects)
+
+class ModelFromJsonTF(ModelBuilder):
+    """ModelBuilder class that builds from model architecture specified
+        in a JSON file. Uses Tensorflow and builds the model on the 
+        specified GPU.
+        Attributes:
+            filename: path to JSON file specifying model architecture
+            device: name of the device to use (ex: "/gpu:2")
+    """
+
+    def __init__(self, comm, filename=None, json_str=None, device_name='cpu', 
+            custom_objects={}):
+        self.filename = filename
+        self.json_str = json_str
+	self.custom_objects = custom_objects
+        self.device = self.get_device_name(device_name)
+        super(ModelFromJsonTF, self).__init__(comm)
+
+    def get_device_name(self, device):
+        """Returns a TF-style device identifier for the specified device.
+            input: 'cpu' for CPU and 'gpuN' for the Nth GPU on the host"""
+        if device == 'cpu':
+            dev_num = 0
+            dev_type = 'cpu'
+        elif device.startswith('gpu'):
+            try:
+                dev_num = int(device[3:])
+                dev_type = 'gpu'
+            except ValueError:
+                print "GPU number could not be parsed from {}; using CPU".format(device)
+                dev_num = 0
+                dev_type = 'cpu'
+        else:
+            print "Please specify 'cpu' or 'gpuN' for device name"
+            dev_num = 0
+            dev_type = 'cpu'
+        return get_device_name(dev_type, dev_num, backend='tensorflow')
+
+    def build_model(self):
+        import tensorflow as tf
+        with tf.device(self.device):
+            model = load_model(filename=self.filename, json_str=self.json_str, 
+                    custom_objects=self.custom_objects)
+        return model
