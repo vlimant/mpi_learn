3916af9f1e40e41c106e9622637f4a065956a17a
Jean-Roch Vlimant <vlimant@cern.ch>
2018-05-03 13:45:20 +0200

    kfolding mpi manager

--- mpi_learn/mpi/manager.py
+++ mpi_learn/mpi/manager.py
@@ -83,7 +83,39 @@ def get_device(comm, num_masters=1, gpu_limit=-1, gpu_for_master=False):
         #print ("Found",dev,"free")
         return dev
                                                                                         
+            
+class MPIKFoldManager(MPIManager):
+    def __init__( self, NFolds, comm, data, algo, model_builder, num_epochs, train_list, 
+                  val_list, num_masters=1, synchronous=False, callbacks=[], 
+                  worker_callbacks=[], verbose=False, custom_objects={}):
+        if NFolds <=1:
+            print ("this is a bit dumb, but we can do it")
+        
+        if int(comm.Get_size() / float(NFolds))<=1:
+            print ("There is less than one master+one worker per fold, this isnt' going to work")
+            
+        rank = comm.rank()
+        fold_num = int(rank * NFolds / comm.Get_size())
+        comm_fold = comm.Split(fold_num)
+        print ("For node {0}, with block rank {1}, send in fold {2}".format(MPI.COMM_WORLD.Get_rank(), rank, fold_num))
+        self.manager = None
+        if comm_fold.Get_Rank() == 0:
+            ## massage the train_list and val_list one way or the other
+            ## my opinion is that the manager should be passed a train list, and it makes the split, train/val
+            ## to be FIXED
+            train_list_on_fold = train_list
+            val_list_on_fold = val_list
+            self.manager = 
+                MPIManager(comm_fold, data, algo, model_builder, num_epochs, train_list_on_fold,
+                           val_list_on_fold, num_masters, synchronous, callbacks,
+                           worker_callbacks, verbose, custom_objects)
+                
+        def train(self):
+            ## this call should start the training of all managers
+            if self.manager:
+                self.manager.train()
     
+
 class MPIManager(object):
     """The MPIManager class defines the topology of the MPI process network
         and creates master and worker objects for each process accordingly.
@@ -206,6 +238,9 @@ class MPIManager(object):
                     num_epochs=self.num_epochs, data=self.data, algo=self.algo, 
                     model_builder=self.model_builder, callbacks=self.worker_callbacks, 
                     verbose=self.verbose, custom_objects=self.custom_objects)
+
+    def train(self):
+        return self.process.train()
 
     def get_num_sync_workers(self, comm):
         """Returns the number of workers the master should wait for
