dcccfed1e02c21c396aecb195074c16df213581f
Jean-Roch Vlimant <vlimant@cern.ch>
2018-06-13 06:40:55 -0700

    mnist torch model

--- models/Models.py
+++ models/Models.py
@@ -1,16 +1,28 @@
 ### Predefined Keras models
 
 import numpy as np
-from keras.models import Sequential
-from keras.layers import Dense, Activation, Dropout, Flatten
-from keras.layers import Convolution2D, MaxPooling2D
-import keras.backend as K
+try:
+    from keras.models import Sequential
+    from keras.layers import Dense, Activation, Dropout, Flatten
+    from keras.layers import Convolution2D, MaxPooling2D
+    import keras.backend as K
+except:
+    print ("no keras support")
+    
+try:
+    import torch
+    import torch.nn as nn
+    import torch.nn.functional as F
+except:
+    print ("no torch support")
+
 
 def make_model(model_name):
     """Constructs the Keras model indicated by model_name"""
     model_maker_dict = {
             'example':make_example_model,
             'mnist':make_mnist_model,
+            'mnist_torch':make_mnist_torch_model,
             }
     return model_maker_dict[model_name]()
 
@@ -59,3 +71,26 @@ def make_mnist_model(**args):
     model.add(Dense(nb_classes))
     model.add(Activation('softmax'))
     return model
+
+class MNistNet(torch.nn.Module):
+    def __init__(self):
+        super(MNistNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+        
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+    
+def make_mnist_torch_model(**args):
+    model = MNistNet()
+    return model
+    
