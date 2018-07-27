91b3809c83e0b5a234b08b866c0862b133f7113f
Filipe <filipepgm@hotmail.com>
2018-07-27 15:25:37 +0200

    single process validation using master validation

--- mpi_learn/mpi/single_process.py
+++ mpi_learn/mpi/single_process.py
@@ -5,7 +5,7 @@ import time
 
 from ..train.monitor import Monitor
 from ..utils import Error, weights_from_shapes, shapes_from_weights
-from .process import MPIWorker
+from .process import MPIWorker, MPIMaster
 
 class MPISingleWorker(MPIWorker):
     """This class trains its model with no communication to other processes"""
@@ -101,75 +101,5 @@ class MPISingleWorker(MPIWorker):
         """Compute the loss on the validation data.
             Return a dictionary of validation metrics.
             Shamelessly copied from MPIMaster"""
-        #return MPIMaster.validate(self)
-
-        tell = True
-        if self.has_parent:
-            return {}
-        #self.model.set_weights(self.weights)
-        if tell: print ("Starting validation")
-        val_metrics = np.zeros((1,))
-        i_batch = 0
-        for i_batch, batch in enumerate(self.data.generate_data()):
-            new_val_metrics =  self.model.test_on_batch(x=batch[0], y =batch[1] )
-            if val_metrics.shape != new_val_metrics.shape:
-                val_metrics =  np.zeros(new_val_metrics.shape)
-            val_metrics += new_val_metrics
-            if self._short_batches and i_batch>self._short_batches: break
-        val_metrics = val_metrics / float(i_batch+1)
-        l = self.model.get_logs(val_metrics, val=True)
-        self.update_history( l )
-
-        if self.target_metric:
-            m,opp,v = self.target_metric
-            model,m= m.split(':') if ':' in m else (None,m)
-            ##print ("target validation",model,m,opp,v)
-            r = self.history_key()
-            use = self.histories[r].get(model,None) if model else self.histories[r]
-            if use:
-                if m in use and ((opp=='>' and use[m][-1]>v) or (opp=='<' and use[m][-1]<v)):
-                    print ("metric",m,"is",opp,v,"stop training")
-                    self.stop_training = True
-            else:
-                print ("fatal target stopping cannot get",m)
-                import sys
-                sys.exit(14)
-        if self.patience:
-            m,opp,p = self.patience
-            p = int(p)
-            model,m=m.split(':') if ':' in m else (None,m)
-            ##print ("patience validation",model,m,opp,p)
-            r = self.history_key()            
-            use = self.histories[r].get(model,None) if model else self.histories[r]
-            ref = None
-            smooth = False
-            if use:
-                if m in use:
-                    ##print ("using",use[m])
-                    if '~' in opp:
-                        smooth = True
-                        opp = opp.replace('~','')
-                        ## do the averaging
-                        if len(use[m])>=(2*p):
-                            ref = np.mean(use[m][-(2*p):-p])
-                            current = np.mean(use[m][-p:])
-                        else:
-                            if len(use[m])>=p:
-                                ref = use[m][-p]
-                                current = use[m][-1]
-                    if ref is not None and current is not None and ((ref<current and opp=='<') or (ref>current and opp=='>')):
-                        print ("metric",m,"is over",p,"patience boundary:",ref,"(ref)",opp,current,"(current)","with smoothing" if smooth else "")
-                        self.stop_training = True
-                else:
-                    print ("fatal early stopping cannot get",m)
-                    import sys
-                    sys.exit(14)                    
-            else:
-                print ("fatal early stopping cannot get",m)
-                import sys
-                sys.exit(14)                
-        print ("Validation metrics:")
-        self.print_metrics(val_metrics)
-        if tell: print ("Ending validation")
-        return None
+        return MPIMaster.validate(self)
 
