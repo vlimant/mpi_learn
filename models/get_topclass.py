735c68053ede78f7eb926672082a5f46749f86df
Jean-Roch Vlimant <vlimant@cern.ch>
2019-02-08 07:01:00 -0800

    variable length input lists

--- models/get_topclass.py
+++ models/get_topclass.py
@@ -1,5 +1,7 @@
 import os
 import glob
+import sys
+
 dest='/bigdata/shared/LCDJets_Abstract_IsoLep_lt_20'
 import socket
 host = os.environ.get('HOST', os.environ.get('HOSTNAME',socket.gethostname()))
@@ -8,5 +10,16 @@ if 'titan' in host:
 train = glob.glob(dest+'/train/*.h5')
 test  = glob.glob(dest+'/val/*.h5')
 
-open('train_topclass.list','w').write( '\n'.join(sorted( train[:10] )))
-open('test_topclass.list','w').write( '\n'.join(sorted( test[:2] )))
+N=10
+Nt=N/5
+if len(sys.argv)>=1:
+    a = sys.argv[1]
+    if a.isdigit():
+        N = int(a)
+        Nt=N/5            
+    else:
+        N,Nt = map(int, a.split(','))
+
+
+open('train_topclass.list','w').write( '\n'.join(sorted( train[:N] )))
+open('test_topclass.list','w').write( '\n'.join(sorted( test[:Nt] )))
